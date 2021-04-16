0cfb52ee4312bb71fb4138f5b4aa968b42327ab9
Enomartins <enowebservices@gmail.com>
2021-04-16 17:23:24 +0100

    all routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "This is the About page";
+})->name('about');
+
+Route::get('/tasks', 'TasksController@index')->name('task.index');
+Route::post('/tasks', 'TasksController@store')->name('task.store');
+Route::get('/tasks/create', 'TasksController@create')->name('task.create');
+Route::get('/tasks/edit/{taskid}', 'TasksController@edit')->name('task.edit');
+Route::put('/tasks/edit/{taskid}', 'TasksController@update')->name('task.update');
+Route::delete('/tasks/edit/{taskid}', 'TasksController@destroy')->name('task.destroy');
